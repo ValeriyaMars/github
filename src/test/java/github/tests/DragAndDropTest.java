7aeaf468b7f1890c2d1376ef0cb684877b5ad59d
valeriya.solovova <valeriya.solovova@simbirsoft.com>
2022-08-18 10:33:30 +0400

    Added GithubTests and DragAndDropTest

---
+++ src/test/java/github/tests/DragAndDropTest.java
@@ -0,0 +1,18 @@
+package github.tests;
+
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class DragAndDropTest {
+
+    @Test
+    void dragAndDropElementAToElementB() {
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        $("#column-a").dragAndDropTo("#column-b");
+        $("#column-b").shouldHave(text("A"));
+    }
+
+}
